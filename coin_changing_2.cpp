c0526c6f40fe2eb69fdff6fcc63f68e4d871a630
Harshit <hg3994@gmail.com>
2018-10-22 22:07:47 +0530

    LCA and coin changing 2

---
+++ coin_changing_2.cpp
@@ -0,0 +1,79 @@
+/* 
+Harshit Gupta | 22nd October, 2018
+
+cpp.sh/6mize
+https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
+
+C++ program for " There a n coins present. We want to make a sum  s from these coins. Can we make it?"
+
+Solution: We will use DP to solve this. At every stage, we have two choices:
+        a. we are NOT considering the nth coin (dp[i-1][j])
+        b. we are considering the nth coin (dp[i-1][j-coins[i-1]])
+        NOTE that if sum amount is less than the coin value, then we have the only choice of NOT considering that coin.
+        Base cases would be when sum=0 and coins are present(we can always make then) and when coins=0 and sum is present( we can necer make)
+        We will build the dp[][] table with help of these cases.
+        
+        This is different from the the coin changing problem because here we DO NOT have infinte supply of coins. 
+        The only coins that we have are the ones in the array
+        
+Time Complexity: O(n2)
+
+Paradigm: Dynamic Programming
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+void coinsChanging(int *coins, int n, int sum){
+    int dp[n+1][sum+1];
+    
+    // When the number of coins are 0 and sum is 0, then the number of ways
+    // we can make it is 1.
+    dp[0][0] = 0;
+    
+    // When the sum is 0, there is 1 way to get the number of coins.
+    for(int i=0;i<n+1; ++i)
+        dp[i][0] = 1;
+        
+    // When the coins are 0, there are 0 ways to make them.
+    for(int i=0;i<sum+1;i++)
+        dp[0][i] = 0;
+        
+    // Iterate over to fill the 2D array.
+    // A sum is possible only if 
+    // a. we are NOT considering the nth coin (dp[i-1][j])
+    // b. we are considering the nth coin (dp[i-1][j-coins[i-1]])
+    // NOTE that if sum amount is less than the coin value, then we have the only choice of NOT considering that coin.
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(j<coins[i-1])
+                dp[i][j] = dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j] || dp[i-1][j-coins[i-1]];
+        }
+    }
+    
+    // Uncomment to print the 2D matrix
+    // for (int i = 0; i <= n; i++) 
+    //  { 
+    //   for (int j = 0; j <= sum; j++) 
+    //       printf ("%4d", dp[i][j]); 
+    //   printf("\n"); 
+    //  }
+    
+    if (dp[n][sum]==1)
+        cout<<"It is possible to make a sum of these coins"<<endl;
+    else
+        cout<<"It is NOT possible to make a sum of these coins"<<endl;
+}
+
+
+int main(){
+    int coins[] = {3, 34, 4, 12, 5, 2}; 
+    int sum = 9; 
+    // int sum = 1; //not possible 
+    int n = sizeof(coins)/sizeof(coins[0]); 
+    coinsChanging(coins, n, sum);
+    return 0;
+}
---
+++ lowest_common_ancestor_of_a_binary_search_tree.cpp
@@ -0,0 +1,76 @@
+/* 
+Harshit Gupta | 22nd October, 2018
+
+cpp.sh/9us6
+https://www.geeksforgeeks.org/lowest-common-ancestor-in-a-binary-search-tree/
+
+C++ program for finding the lowest common ancestor for two nodes of a Binary Search Tree.
+
+Solution: We will use the property of BST to get the solution faster. 
+    To every node starting from root, 
+    
+        If both n1 and n2 are lesser than the root, 
+            then we know that both the nodes must lie in the left subtree, so we recur there
+        If both n1 and n2 are greater than the root node,
+            then we know that both the nodes must lie in the right subtree, so we recur there.
+        If it doesn't satisfy both the above cases, this means that one of the node is lesser than it 
+            and one is larger than it, so we return the 'root' at that instance, since that would be the LCA.
+            
+Time Complexity: O(height) = O(logn) on average(usually) = O(n) in worst case(skewed).
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    struct node *left, *right;
+    
+    node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node* lowestCommonAncestorBST(node *root, node *n1, node *n2){
+    
+    // If the root is NULL, then return NULL.
+    if (root==NULL)
+        return NULL;
+        
+    // If n1's data and n2's data are smaller than the root's data, 
+    // then LCA must lie in the left subtree
+    if(root->data > n1->data && root->data > n2->data)
+        return lowestCommonAncestorBST(root->left, n1, n2);
+
+    // If n1's data and n2's data are larger than the root's data, 
+    // then LCA must lie in the right subtree
+    else if(root->data < n1->data && root->data < n2->data)
+        return lowestCommonAncestorBST(root->right, n1, n2);
+    
+    // Else the n1's data must be smaller than the root's data
+    // and n2's data must be larger than the root's data.
+    // This is the LCA of n1 and n2.
+    else
+        return root;
+}
+
+int main() {
+    struct node *root        = new node(20); 
+    root->left               = new node(8); 
+    root->right              = new node(22); 
+    root->left->left         = new node(4); 
+    root->left->right        = new node(12); 
+    root->left->right->left  = new node(10); 
+    root->left->right->right = new node(14); 
+    
+    node *lca = lowestCommonAncestorBST(root, root->left->right , root->right); //LCA of 12 and 22
+    if(lca!=NULL)
+        cout<<lca->data<<" is the LCA of "<<root->left->right->data<<" and "<<root->right->data<<endl;
+    else
+        cout<<"Wrong Input"<<endl;
+	return 0;
+}
